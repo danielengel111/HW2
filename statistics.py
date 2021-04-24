56d0a44c19101be2c8bc5c41c88007d261ba1fa0
StasEngel <69434834+StasEngel@users.noreply.github.com>
2021-04-24 17:18:45 +0300

    mean and median

--- statistics.py
+++ statistics.py
@@ -1,7 +1,10 @@
-
-
 def mean(values):
-    pass
+    return sum(values) / len(values)
 
 def median(values):
-    pass
+    n = len(values)
+    sorted_values = sorted(values)
+    if n % 2 == 0:
+        return (sorted_values[(n / 2) - 1] + sorted_values[n / 2]) / 2
+    else:
+        return sorted_values[n / 2]
