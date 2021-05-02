1215812b621f6f05da8da96b324fa46a7c6488f2
maayan <maayanmagal@gmail.com>
2021-05-02 18:10:56 +0300

    main

--- districts.py
+++ districts.py
@@ -3,13 +3,30 @@ class Districts:
         self.dataset = dataset
 
     def filter_districts(self, letters):
-        pass
+        districts=self.dataset.get_all_districts()
+        filtered_districts=[]
+        for district in districts:
+            for letter in letters:
+                if(district[0]==letter):
+                    filtered_districts.append(district)
+        self.dataset.set_districts_data(filtered_districts)
 
     def print_details(self, features, statistic_functions):
-        pass
+        for feature in features:
+            print("{}: ".format(feature), end="")
+            feat_list = self.dataset[feature]
+            for x in range(len(statistic_functions) - 1):
+                print("{}, ".format(statistic_functions[x](feat_list)), end="")
+            print("{} ".format((float)(statistic_functions[len(statistic_functions) - 1](feat_list))))
 
     def determine_day_type(self):
-        pass
+        x= {}
+        for line in self.dataset:
+            if(line["resigned_healed"]-line["new_positives"]>0):
+                x.update({"day_type": 1})
+            else:
+                x.update({"day_type":0})
+        self.dataset.data.update(x)
 
     def get_districts_class(self):
         pass
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import sys
-
+import districts as districts
+import data as data_set
 
 def main(argv):
+    covid_data = argv[1]
+    data = data_set.Data(covid_data)
+    district = districts.Districts(data)
+    print(district.filter_districts(['B']))
     pass
 
 
