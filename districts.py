5a225bba5a64dfd57cc36772b0b69dc263c53ffb
StasEngel <69434834+StasEngel@users.noreply.github.com>
2021-05-03 16:15:17 +0300

    fix get_districts_class

--- districts.py
+++ districts.py
@@ -7,7 +7,7 @@ class Districts:
         filtered_districts=[]
         for district in districts:
             for letter in letters:
-                if district[0]==letter:
+                if district[0] == letter:
                     filtered_districts.append(district)
         self.dataset.set_districts_data(filtered_districts)
 
@@ -21,8 +21,8 @@ class Districts:
 
     def determine_day_type(self):
         self.dataset.data['day_type'] = []
-        for i in range(len(self.dataset.data["resigned_healed"])):
-            if self.dataset.data["resigned_healed"][i] - self.dataset.data["new_positives"][i] > 0:
+        for i in range(len(self.dataset.data['denominazione_region'])):
+            if self.dataset.data['resigned_healed'][i] - self.dataset.data['new_positives'][i] > 0:
                 self.dataset.data['day_type'].append(1)
             else:
                 self.dataset.data['day_type'].append(0)
@@ -30,18 +30,18 @@ class Districts:
     def get_districts_class(self):
         distinct_districts = self.dataset.get_all_districts()
         self.determine_day_type()
-        dict = {}
-        dict['green'] = []
-        dict['not_green'] = []
+        new_dict = {}
+        new_dict['green'] = []
+        new_dict['not_green'] = []
 
         for district in distinct_districts:
             green_days = 0
             for i in range(len(self.dataset.data['hospitalized_with_symptoms'])):
-                if self.dataset.data['denominazione_region'] == district and self.dataset.data['day_type'][i] == 1:
+                if self.dataset.data['denominazione_region'][i] == district and self.dataset.data['day_type'][i] == 1:
                     green_days += 1
             if green_days > 340:
-                dict['green'].append(district)
+                new_dict['green'].append(district)
             else:
-                dict['not_green'].append(district)
-        return dict
+                new_dict['not_green'].append(district)
+        return new_dict
 
